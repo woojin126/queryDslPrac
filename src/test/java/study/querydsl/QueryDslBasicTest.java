dc9e60018d061b205540cc2c5a4b6f4ddab96fda
woojin126 <woojin126@naver.com>
2021-08-03 05:54:12 +0900

    queryDsl Test  ---- 목록 List조회 , 단건조회 , 페이징속성

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -1,24 +1,34 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
+import org.hibernate.query.criteria.internal.expression.function.AggregationFunction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 public class QueryDslBasicTest {
 
     @PersistenceContext
@@ -43,6 +53,39 @@ public class QueryDslBasicTest {
         em.persist(member2);
         em.persist(member3);
         em.persist(member4);
+    }
+
+    @Test
+    void querydsl(){
+   /*     List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .where(member.age.between(20, 40).or(member.username.eq("member1")))
+                .orderBy(member.age.desc())
+                .fetch();
+
+        for (Member fetch1 : fetch) {
+            System.out.println("fetch1.getUsername() = " + fetch1.getUsername());
+        }
+    */
+
+/*
+       queryFactory
+                .select(member.age.avg(), team.id)
+                .from(member)
+                .innerJoin(team)
+                .on(member.team.id.eq(team.id))
+                .where(member.id.gt(10))
+                .fetch();*/
+
+        List<Tuple> fetch = queryFactory
+                .select(member.age.avg(), member.username)
+                .from(member)
+                .where(member.id.goe(3))
+                .groupBy(member.username)
+                .orderBy(member.username.asc())
+                .fetch();
+
+
     }
 
     @Test
@@ -92,5 +135,38 @@ public class QueryDslBasicTest {
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch(){
+  /*      List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();  //맴버의 목록을 List로 조회
+
+        Member fetchOne = queryFactory
+                .selectFrom(QMember.member)
+                .fetchOne(); //단건조회
+
+        Member fetchFirst = queryFactory
+                .selectFrom(QMember.member)
+                //.limit(1).fetchOne()
+                .fetchFirst();//위와 같은코드  단건조회
+    */
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        //results.getTotal();
+        List<Member> content = results.getResults(); // 데이터가 나옴 이렇게해야
+        //results.getLimit();
+        //results.getOffset();
+
+
+
+       //카운트만가져옴
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+
     }
 }
