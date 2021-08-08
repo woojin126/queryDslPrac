26cd65fec2f5c52f7a075ff6049b65e370ae889e
woojin126 <woojin126@naver.com>
2021-08-09 00:57:07 +0900

    CustomRepository 주석설명

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -5,7 +5,11 @@ import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member,Long> {
+/**
+ * 여기서 MemberRepositoryCustom을 상속했는데 이렇게하면 너무 복잡해질수있다.
+ * 그러면그냥 @Repository 클래스를 따로만들어서 하는것도 상황에따라 고려하면서 설계(그냥아주 특화된 쿼리일경우!)
+ */
+public interface MemberRepository extends JpaRepository<Member,Long>,MemberRepositoryCustom {
 
     //select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
