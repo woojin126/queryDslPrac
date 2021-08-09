dd99db9bca4aa1979c521aca10f1ece1f0db1860
woojin126 <woojin126@naver.com>
2021-08-09 21:02:12 +0900

    커스텀 QuerydslRespositorySupport 추가 ( 페이징 편하게 하기위해)

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,14 +2,19 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,10 +23,15 @@ import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
 //클래명이 중요 MemberRepository + Impl 규칙임
-@RequiredArgsConstructor
-public class MemberRepositoryImpl implements MemberRepositoryCustom{
+public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
+
+
+    public MemberRepositoryImpl(Class<?> domainClass, JPAQueryFactory queryFactory) {
+        super(domainClass);
+        this.queryFactory = queryFactory;
+    }
 
     public List<MemberTeamDto> search(MemberSearchCondition condition){
         return queryFactory
@@ -90,7 +100,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 ).offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 //ageBetween(condition))
-                .fetchResults();//컨텐츠용쿼리, count용 쿼리 2번날림
+                .fetchResults();//컨텐츠용쿼리, count용 쿼리 2번날림,데이터가 얼마없으면 그냥쓰꼬 아니면 아래 최적화
 
         List<MemberTeamDto> content = result.getResults();//실제 데이터
         long total = result.getTotal();//count 숫자
@@ -98,7 +108,8 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
         return new PageImpl<>(content,pageable,total);
     }
 
-    @Override
+
+        @Override
     public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
         List<MemberTeamDto> content = queryFactory
                 .select(new QMemberTeamDto(
@@ -118,8 +129,13 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 .limit(pageable.getPageSize())
                 //ageBetween(condition))
                 .fetch();//컨텐츠용쿼리, count용 쿼리 2번날림
-
-        long count = queryFactory
+/**
+ * 스프링 데이터 라이브러리 제공
+ * count 쿼리가 생략 가능한 경우 생략해서 처리 (최적화 를위해)
+ * 1.페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을때
+ * 2.마지막 페이지 일 때(offset + 컨텐츠 사이즈를 더해서 사이즈를 구함)
+ */
+        JPAQuery<Member> countQuery = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
@@ -128,9 +144,20 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                ).fetchCount();
+                );
+        /*long count = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ).fetchCount();*/
 
-        return new PageImpl<>(content,pageable,count);
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount); // 카운트가 필요없음 최적화이걸쓰짜,이구문이 실행될떄 카운트쿼리를날릴지 말지 결정함 알아서
+        //return new PageImpl<>(content,pageable,count);
     }
 
 }
