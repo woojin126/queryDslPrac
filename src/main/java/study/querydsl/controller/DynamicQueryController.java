8aad2bdae32a2ee888caeca41334c5c90e03a7fc
woojin126 <woojin126@naver.com>
2021-08-09 21:01:24 +0900

    동적쿼리 Controller 페이징 단계별로 최적화

--- src/main/java/study/querydsl/controller/DynamicQueryController.java
+++ src/main/java/study/querydsl/controller/DynamicQueryController.java
@@ -1,6 +1,8 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -17,11 +20,20 @@ import java.util.List;
 public class DynamicQueryController {
 
     private final MemberJpaRepository memberJpaRepository;
-
+    private final MemberRepository memberRepository;
 
     @GetMapping("api/v1/members")
     public ResponseEntity<List<MemberTeamDto>> searchType(@RequestBody MemberSearchCondition searchCondition){
        return new ResponseEntity<>(memberJpaRepository.search(searchCondition),HttpStatus.OK);
     }
 
+    @GetMapping("api/v2/members")
+    public ResponseEntity<Page<MemberTeamDto>> searchPageSimple(@RequestBody MemberSearchCondition condition, Pageable pageable){
+        return new ResponseEntity<>(memberRepository.searchPageSimple(condition,pageable),HttpStatus.OK);
+    }
+
+    @GetMapping("api/v3/members")
+    public ResponseEntity<Page<MemberTeamDto>> searchPageComplex(@RequestBody MemberSearchCondition condition, Pageable pageable){
+        return new ResponseEntity<>(memberRepository.searchPageComplex(condition,pageable),HttpStatus.OK);
+    }
 }
